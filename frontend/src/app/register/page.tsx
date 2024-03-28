07d81de3c58880909a312532ee94d9c44b30efbd
arjaybudzz <archessonan.1@gmail.com>
2024-03-28 18:04:08 +0800

    added register page

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -4,18 +4,21 @@ export default function Register() {
 	return (
 
 		<div className="flex justify-center items-center w-screen h-screen">
-			<form className="flex flex-col justify-around items-center w-1/4 h-3/4 bg-gray-300">
-				<h1 className="text-4xl font-bold">
-					Register	
-				</h1>	
+			<form className="flex flex-col justify-around items-center shadow-md rounded-xl shadow-black w-1/4 h-3/4 p-6 bg-gray-300">
+				<div className="flex w-full h-auto justify-center mb-5 items-center">
+					<h1 className="text-4xl font-bold">
+						Register	
+					</h1>	
+				</div>	
 				
-				
-				<div className="flex flex-col w-full h-full">
+				<div className="flex flex-col w-full h-full gap-4">
 					<TextField label="Username" type="text" name="username-input" required/>
 					<TextField label="Email" type="email" name="email-input" required/>
 					<TextField label="Password" type= "password" name="password-input" required/>
 					<TextField label="Confirm Password" type="password" name="password-confirmation-input" required/>
 				</div>
+
+				<button type="submit" className="w-full h-14 bg-black text-white font-bold">SUBMIT</button>
 			</form>	
 		</div>
 	);

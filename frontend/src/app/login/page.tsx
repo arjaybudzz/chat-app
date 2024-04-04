cce4a1b262ca16367d558779ccd97d562ab6cf5c
arjaybudzz <archessonan.1@gmail.com>
2024-04-04 20:50:30 +0800

    implemented login function

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,15 +1,40 @@
+"use client"
+
 import TextField from '@mui/material/TextField'
 import SubmitButton from "../components/SubmitButton"
 import Link from 'next/link'
 import { login } from "../api/user"
 import axios from 'axios';
-
-
-
+import { useRouter } from 'next/navigation'
+import { z } from 'zod';
+import { cookies } from 'next/cache';
 
 export default function Login() {
-	
 
+	const router = useRouter();	
+
+	const user = z.object({
+		email: z.string().email(),
+		password: z.string()
+
+	}).required().strict();
+
+
+	const login = async(formData: FormData) => {
+		
+		const result = user.parse({
+			email: formData.get("email-input"),
+			password: formData.get("password-input")
+		});
+
+		await axios.post(`http://127.0.0.1:3001/api/v1/tokens?user[email]=${result.email}&user[password]=${result.password}`)
+		.then((response) => {
+
+			cookies().set("userToken", response.data.token);
+			cookies().set("userId", response.data.id);
+			router.push("/feed");
+		}).catch((errors) => console.log(errors)),
+	}
 
 	return (
 		<div className="flex justify-center items-center w-screen h-screen">
